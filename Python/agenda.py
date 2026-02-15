6df2bcb5baac31594de98f8aa55121d46340a687
Rogério Alves <rogerio.alves651@gmail.com>
2026-02-15 16:10:22 -0300

    Feito;

--- Python/agenda.py
+++ Python/agenda.py
@@ -1,8 +1,8 @@
 import sqlite3
-conexao = sqlite3.connect("agenda.db")
+conexao = sqlite3.connect("agenda2.db")
 cursor = conexao.cursor()
-cursor.execute('''create table agenda(nome text, telefone text)''')
-cursor.execute('''insert into agenda (nome, telefone) values(?, ?)''', ("Nilo", "7788-1432"))
+cursor.execute('''create table agenda2(nome text, telefone text)''')
+cursor.execute('''insert into agenda2 (nome, telefone) values(?, ?)''', ("Nilo", "7788-1432"))
 conexao.commit()
 cursor.close()
 conexao.close()
